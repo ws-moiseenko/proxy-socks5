60961e16b96cd653b35674fe801e8cc9636603be
Alex Moiseenko <ws.moiseenko@gmail.com>
2018-04-13 14:24:17 +0300

    fix

--- server.js
+++ server.js
@@ -3,3 +3,26 @@
 var
 	socks5 = require('simple-socks'),
 	server = socks5.createServer().listen(1080);
+
+// When a reqest arrives for a remote destination
+server.on('proxyConnect', function (info, destination) {
+	console.log('connected to remote server at %s:%d', info.host, info.port);
+
+	destination.on('data', function (data) {
+		console.log(data.length);
+	});
+});
+
+server.on('proxyData', function (data) {
+	console.log(data.length);
+});
+
+server.on('proxyError', function (err) {
+	console.error('unable to connect to remote server');
+	console.error(err);
+});
+
+server.on('proxyEnd', function (response, args) {
+	console.log('socket closed with code %d', response);
+	console.log(args);
+});
